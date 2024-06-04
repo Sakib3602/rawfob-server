8169090c6f0a74de52d538cf2c207bc647507fb3
Sakib3602 <sakibsarker6969@gmail.com>
2024-06-04 17:22:16 +0600

    finnaly fix the bug

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
-const port =  9000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 9000;
 
-app.use(cors())
-app.use(express.json()); 
-require('dotenv').config()
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
 
-
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.b5jufhp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,48 +26,80 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     const mainPosts = client.db("lastAssingment").collection("post");
-    
+    const commentsDB = client.db("lastAssingment").collection("comment");
 
-    
-    app.get('/posts', async(req,res)=>{
+    app.get("/posts", async (req, res) => {
+      const filter = req.query;
 
-      const filter = req.query
+      console.log(filter);
 
-      console.log(filter)
+      // for search
       const query = {
-        tag:{$regex : filter.search , $options : 'i'}
-      }
+        tag: { $regex: filter.search, $options: "i" },
+      };
+      // for sort
       const options = {
         sort: {
-          upvote : filter.sort === "asc" ? 1 : -1 
-        }
-      }
-    
-    
-      const cursor = await mainPosts.find(query,options).toArray();
-      // console.log(cursor)
-      res.send(cursor)
-    })
+          upvote: filter.sort === "asc" ? 1 : -1,
+        },
+      };
 
-    app.get("/posts/:id" , async(req,res)=>{
-      const id = req.params.id
+      const cursor = await mainPosts.find(query, options).toArray();
+      // console.log(cursor)
+      res.send(cursor);
+    });
+
+    app.get("/posts/:id", async (req, res) => {
+      const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const movie = await mainPosts.findOne(query);
-      res.send(movie)
-    })
 
+      res.send(movie);
+    });
 
+    // update post
+    app.put("/posts/:id", async (req, res) => {
+      const postId = req.params.id;
+      const doc = req.query
+      console.log(doc)
+   
 
+      const update = {};
+      if (doc.vote === "true") {
+        update.$inc = { upvote: 1 };
+      } else {
+        update.$inc = { downvote: 1 };
+      } 
+      
 
+     
+      const updatedPost = await mainPosts.findOneAndUpdate(
+        { _id: new ObjectId(postId) },
+        update,
+        { new: true }
+      );
 
+     
+      // console.log(updatedPost);
 
+      res.send(updatedPost);
+    });
+    // update post end
 
+    // comment
 
-
-
+    // comment post api
+    app.post("/comments", async (req, res) => {
+      const doc = req.body;
+      console.log(doc);
+      const result = await commentsDB.insertOne(doc);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -78,11 +107,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
