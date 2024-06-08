f9be5420c718073615cad4a1fa1e47a96e7908e4
Sakib3602 <sakibsarker6969@gmail.com>
2024-06-08 11:43:32 +0600

    pagination added with size and limit

--- index.js
+++ index.js
@@ -30,46 +30,46 @@ async function run() {
     const userDB = client.db("lastAssingment").collection("userData");
     const announceDB = client.db("lastAssingment").collection("announce");
 
-
     // main post works
     app.get("/posts", async (req, res) => {
       const filter = req.query;
 
       console.log(filter);
+      const page = parseInt(filter.page);
+      const size = parseInt(filter.size);
+
+      console.log(page, size);
 
       // for search
       const query = {
         tag: { $regex: filter.search, $options: "i" },
       };
-      // for sort
-      const options = {
-        sort: {
-          popularity: filter.sort === "asc" ? 1 : -1,
-        },
-      };
-
 
       const sortOrder = filter.sort === "asc" ? 1 : -1;
       // aggregate
-     
-     const agg = await mainPosts.aggregate([
-      {
-      
-          $match: query
-      
 
-      },
-     
+      const agg = await mainPosts
+        .aggregate([
+          {
+            $skip: page * size,
+          },
+          {
+            $limit: size,
+          },
+          {
+            $match: query,
+          },
 
-        {
-        $addFields: {
-        popularity: { $subtract: ['$upvote', '$downvote'] }
-        }
-        },
-        {
-          $sort: { popularity: sortOrder }
-      }
-        ]).toArray()
+          {
+            $addFields: {
+              popularity: { $subtract: ["$upvote", "$downvote"] },
+            },
+          },
+          {
+            $sort: { popularity: sortOrder },
+          },
+        ])
+        .toArray();
 
       // const cursor = await mainPosts.find(query, options).toArray();
       // res.send({agg,cursor})
@@ -85,57 +85,49 @@ async function run() {
     });
     app.get("/postss/:email", async (req, res) => {
       const email = req.params.email;
-      const query = { email : email };
-      console.log(email)
+      const query = { email: email };
+      console.log(email);
       const movie = await mainPosts.find(query).toArray();
 
       res.send(movie);
     });
 
-    app.post("/posts", async(req,res)=> {
-      const data = req.body
-      console.log(data)
+    app.post("/posts", async (req, res) => {
+      const data = req.body;
+      console.log(data);
 
-       const result = await mainPosts.insertOne(data);
-       res.send(result)
-
-    })
-    app.delete("/posts/:id", async(req,res)=>{
-      const dlt = req.params.id
-      const query = {_id : new ObjectId(dlt)}
+      const result = await mainPosts.insertOne(data);
+      res.send(result);
+    });
+    app.delete("/posts/:id", async (req, res) => {
+      const dlt = req.params.id;
+      const query = { _id: new ObjectId(dlt) };
       const result = await mainPosts.deleteOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // update post
     app.put("/posts/:id", async (req, res) => {
       const postId = req.params.id;
-      const doc = req.query
-      console.log(doc)
-   
+      const doc = req.query;
+      console.log(doc);
 
       const update = {};
       if (doc.vote === "true") {
         update.$inc = { upvote: 1 };
       } else {
         update.$inc = { downvote: 1 };
-      } 
+      }
 
-
-     
       const updatedPost = await mainPosts.findOneAndUpdate(
         { _id: new ObjectId(postId) },
         update,
         { new: true }
       );
 
+      // subtraction
 
-      // subtraction 
-      
-        
       // subtraction end
-     
-    
 
       res.send(updatedPost);
     });
@@ -150,97 +142,78 @@ async function run() {
       const result = await commentsDB.insertOne(doc);
       res.send(result);
     });
-    app.get('/comments/:title', async(req,res)=>{
-      const tit = req.params.title
+    app.get("/comments/:title", async (req, res) => {
+      const tit = req.params.title;
       // console.log(tit)
-      const query = { title : tit };
-      console.log(query)
+      const query = { title: tit };
+      console.log(query);
 
       const cursor = await commentsDB.find(query).toArray();
-      res.send(cursor)
-    })
-
+      res.send(cursor);
+    });
 
     // user data get from registration
-    app.post("/userData", async(req,res)=>{
-      const doc = req.body
-      console.log(doc.email)
+    app.post("/userData", async (req, res) => {
+      const doc = req.body;
+      console.log(doc.email);
 
-      const query = {email : doc.email}
-      const isThere = await userDB.findOne(query)
-      if(isThere){
-        return res.send({massege : 'Sorry All ready there'})
+      const query = { email: doc.email };
+      const isThere = await userDB.findOne(query);
+      if (isThere) {
+        return res.send({ massege: "Sorry All ready there" });
       }
-      console.log(doc)
+      console.log(doc);
       const result = await userDB.insertOne(doc);
-      res.send(result)
-
-    })
+      res.send(result);
+    });
 
     // find by email
 
-    app.get("/userData/:email" , async(req,res)=> {
-      const doc = req.params.email
-      console.log(doc)
-      const query = {email : doc}
-      const result = await userDB.findOne(query)
-      res.send(result)
-
-    })
-
-
-
+    app.get("/userData/:email", async (req, res) => {
+      const doc = req.params.email;
+      console.log(doc);
+      const query = { email: doc };
+      const result = await userDB.findOne(query);
+      res.send(result);
+    });
 
     // dashbord
     // admin stats
 
-    app.get("/admin-stats", async(req,res)=>{
-      const totalUsers = await userDB.estimatedDocumentCount()
-      const totalPosts = await mainPosts.estimatedDocumentCount()
-      const totalComments = await commentsDB.estimatedDocumentCount()
-      const totalAnouncement = await announceDB.estimatedDocumentCount()
-       
-      res.send({totalUsers,totalPosts,totalComments,totalAnouncement})
-    })
+    app.get("/admin-stats", async (req, res) => {
+      const totalUsers = await userDB.estimatedDocumentCount();
+      const totalPosts = await mainPosts.estimatedDocumentCount();
+      const totalComments = await commentsDB.estimatedDocumentCount();
+      const totalAnouncement = await announceDB.estimatedDocumentCount();
 
-    app.post("/announcement" , async(req,res)=>{
+      res.send({ totalUsers, totalPosts, totalComments, totalAnouncement });
+    });
+
+    app.get("/page", async (req, res) => {
+      const totalPosts = await mainPosts.estimatedDocumentCount();
+
+      res.send({ totalPosts });
+    });
+
+    app.post("/announcement", async (req, res) => {
       const doc = req.body;
-      const result = await  announceDB.insertOne(doc)
-      res.send(result)
-    })
+      const result = await announceDB.insertOne(doc);
+      res.send(result);
+    });
 
-    app.get("/annData", async(req,res)=>{
+    app.get("/annData", async (req, res) => {
       const result = await announceDB.find().toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.delete("/deleteAnn/:id", async(req,res)=>{
-      const id = req.params.id
-      console.log(id,"idddddd")
+    app.delete("/deleteAnn/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id, "idddddd");
 
-      const query = {_id : new ObjectId(id)}
-      const result = await announceDB.deleteOne(query)
-      res.send(result)
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      const query = { _id: new ObjectId(id) };
+      const result = await announceDB.deleteOne(query);
+      res.send(result);
+    });
 
     // -----------------------------------------------------------------------------------------
 
